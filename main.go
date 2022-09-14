8630e37840b3def52b4a9fde2bb45ecc5d89d922
gwokwong <guowang1102@163.com>
2022-09-14 15:37:47 +0800

    any

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
-  "net/http"
+	"fmt"
+	"net/http"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r := gin.Default()
+	// r.GET("/ping", func(c *gin.Context) {
+	//   c.JSON(http.StatusOK, gin.H{
+	//     "message": "pong",
+	//   })
+	// })
+	r.Any("/crud", func(ctx *gin.Context) {
+		//获取请求方法和请求参数
+		params := ctx.GetString("yes")
+		fmt.Print("params-------------》: %s", params)
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
